3543540dd23fa5416f06a77c307f06361e2b85c7
malpisen <malin.rydefalk@gmail.com>
2025-08-04 18:04:00 +0200

    US2: show age of students

--- dist/main.js
+++ dist/main.js
@@ -1,20 +1,15 @@
-type student = {
-    name: string;
-    age: number;
-};
-
-let students: student[] = [
+let students = [
     { name: 'Lisa', age: 15 },
     { name: 'Emil', age: 16 },
     { name: 'Grillbritt', age: 16 }
 ];
-
-const studentList = document.getElementById("student-list") as HTMLUListElement;
+const studentList = document.getElementById("student-list");
 studentList.innerHTML = ""; // Rensa listan
-
 students.forEach(student => {
     const li = document.createElement("li");
     const age = student.age;
     li.textContent = `${student.name} (${age} år)`;
     studentList.appendChild(li);
 });
+export {};
+//# sourceMappingURL=main.js.map
