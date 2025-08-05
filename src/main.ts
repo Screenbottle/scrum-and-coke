47c3d9a7dbd329e3aa63b65a6a3479e155901bfe
Andreas Granér <Andreas@graner.me>
2025-08-05 13:42:35 +0200

    Fix: Added id to student to list items via data attribbutes

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
 type Student = {
   name: string;
+  id: number;
 };
 
 const students: Student[] = [
   {
     name: 'Lisa',
+    id: 1,
   },
   {
     name: 'Emil',
+    id: 2,
   },
   {
     name: 'Grill-britt',
+    id: 3,
   },
 ];
 
@@ -19,5 +23,6 @@ const classList = document.getElementById('student-list') as HTMLUListElement;
 students.forEach((student) => {
   const li = document.createElement('li') as HTMLLIElement;
   li.textContent = student.name;
+  li.dataset.id = student.id.toString();  
   classList.appendChild(li);
 });
