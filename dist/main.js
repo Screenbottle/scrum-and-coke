07f8345a18607f2becca93f333475e845e088957
malpisen <malin.rydefalk@gmail.com>
2025-08-07 14:01:02 +0200

    added renderStudentList-function

--- dist/main.js
+++ dist/main.js
@@ -1,15 +1,52 @@
+const addStudentButton = document.getElementById("add-student");
+const studentNameInput = document.getElementById("student-name");
+const studentAgeInput = document.getElementById("student-age");
 let students = [
-    { name: 'Lisa', age: 15 },
-    { name: 'Emil', age: 16 },
-    { name: 'Grillbritt', age: 16 }
+    {
+        id: 1,
+        name: "Lisa",
+        age: 15,
+        isActive: false,
+    },
+    {
+        id: 2,
+        name: "Emil",
+        age: 16,
+        isActive: true,
+    },
+    {
+        id: 3,
+        name: "Grillbritt",
+        age: 16,
+        isActive: true,
+    },
 ];
-const studentList = document.getElementById("student-list");
-studentList.innerHTML = ""; // Rensa listan
-students.forEach(student => {
-    const li = document.createElement("li");
-    const age = student.age;
-    li.textContent = `${student.name} (${age} år)`;
-    studentList.appendChild(li);
+renderStudentList();
+addStudentButton.addEventListener("click", () => {
+    const studentName = studentNameInput.value.trim();
+    const studentAge = Number(studentAgeInput.value.trim());
+    const newId = students.length > 0 ? Math.max(...students.map(s => s.id)) + 1 : 1;
+    const student = {
+        id: newId,
+        name: studentName,
+        age: studentAge,
+        isActive: true
+    };
+    students.push(student);
+    studentNameInput.value = "";
+    studentAgeInput.value = "";
+    renderStudentList();
 });
+function renderStudentList() {
+    const studentList = document.getElementById("student-list");
+    studentList.innerHTML = ""; // Rensa listan
+    students.forEach(student => {
+        const li = document.createElement("li");
+        const age = student.age;
+        li.textContent = `${student.name} (${age} år)`;
+        li.dataset.id = student.id.toString();
+        studentList.appendChild(li);
+    });
+}
 export {};
 //# sourceMappingURL=main.js.map
